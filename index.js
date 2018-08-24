c40a8e8458eeedbc61063e11ced901cf63092b1e
Jonah Meijers <jonahmeijers97@gmail.com>
2018-08-24 13:28:31 +0200

    right text and remove commas on array join

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+process.env["NTBA_FIX_319"] = 1;
 const TelegramBot = require('node-telegram-bot-api')
 
 const botToken = '669342127:AAFExLVizilRvdZt5O01Ut3KQIns6Q41hkw'
@@ -5,9 +6,9 @@ const mockBot = new TelegramBot(botToken, {polling: true});
 
 mockBot.onText(/\/(mock|auti) (.+)/, (msg, match) => {
     const chatId = msg.chat.id
-    const text = match[1].split('')
+    const text = msg.text.substring(5)
     const mockedText = text
         .map((val, i) => i % 2 ? val.toUpperCase() : val)
-        .join()
+        .join(',')
     mockBot.sendMessage(chatId, mockedText)
 })
