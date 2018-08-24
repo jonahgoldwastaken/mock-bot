266ee7f26728e6f7b375603d1604228f7869d8ac
Jonah Meijers <jonahmeijers97@gmail.com>
2018-08-24 22:09:04 +0200

    capitalisation randomised

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const TelegramBot = require('node-telegram-bot-api')
 const botToken = process.env.TELEGRAM_TOKEN
 const mockBot = new TelegramBot(botToken, {polling: true});
 
+const mapCharArray = (total = '', val) => {
+    if (!total.length) return val
+    else if (total[total.length - 1] === total[total.length - 1].toUpperCase()) return total.concat(val)
+    else if (Math.random() < .7) return total.concat(val.toUpperCase())
+    else return total.concat(val)
+}
+
 const mockifyText = text => text
     .split('')
-    .map((val, i) => i % 2 ? val : val.toUpperCase())
-    .join('')
+    .reduce(mapCharArray)
 
 mockBot.onText(/\/(mock|auti) (.+)/, ({ text, chat: { id: chatId }}) => {
     const mockedText = mockifyText(text.substring(6))
@@ -15,11 +21,11 @@ mockBot.onText(/\/(mock|auti) (.+)/, ({ text, chat: { id: chatId }}) => {
 })
 
 mockBot.on('inline_query', e => {
-    const mockedText = mockifyText(e.query) || 'Type something!'
+    const mockedText = e.query.length ? mockifyText(e.query) : mockifyText('Type something!')
     const answer = [{
         type: 'article',
         id: '1',
-        title: 'MoCkIfIeD tExT',
+        title: mockifyText('Mocked text'),
         input_message_content: {
             message_text: mockedText,
             parse_mode: 'Markdown'
@@ -29,4 +35,4 @@ mockBot.on('inline_query', e => {
     mockBot.answerInlineQuery(e.id, answer)
 })
 
-mockBot.on('polling_error', err => { console.log(err) })
+mockBot.on('polling_error', console.error)
