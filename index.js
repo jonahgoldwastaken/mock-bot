2124984269bb5e04a97841c3419f530c29c94fae
Jonah Meijers <jonahmeijers97@gmail.com>
2018-12-23 14:14:00 +0100

    offloaded logic to package

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
 process.env["NTBA_FIX_319"] = 1;
-const TelegramBot = require('node-telegram-bot-api')
+const TelegramBot = require("node-telegram-bot-api");
+const mock = require("mockbot-cli");
 
-require('dotenv').config()
-const botToken = process.env.TELEGRAM_TOKEN
-const mockBot = new TelegramBot(botToken, {polling: true, filepath: false})
+require("dotenv").config();
+const botToken = process.env.TELEGRAM_TOKEN;
+const mockBot = new TelegramBot(botToken, { polling: true, filepath: false });
 
-const mapToLowerCase = val => val.toLowerCase()
-
-const mapCharArray = (total = '', val) => {
-    if (!total.length) return val
-    else if (total[total.length - 1] === total[total.length - 1].toUpperCase()) return total.concat(val)
-    else if (Math.random() < .8) return total.concat(val.toUpperCase())
-    else return total.concat(val)
-}
-
-const mockifyText = text => text
-    .split('')
-    .map(mapToLowerCase)
-    .reduce(mapCharArray)
-
-mockBot.onText(/\/(mock)/, ({from, chat: { id: chatId }, reply_to_message: reply}) => {
-    if (typeof reply !== 'undefined') {
-        const mockedText = mockifyText(reply.text)
-        mockBot.sendMessage(chatId, mockedText)
+mockBot.onText(
+  /\/(mock)/,
+  ({ from, chat: { id: chatId }, reply_to_message: reply }) => {
+    if (typeof reply !== "undefined") {
+      const mockedText = mock(reply.text);
+      mockBot.sendMessage(chatId, mockedText);
     } else {
-        mockBot.sendMessage(chatId, mockifyText(`Vergeet je niet iets, ${from.first_name}?`))
+      mockBot.sendMessage(
+        chatId,
+        mock(`Vergeet je niet iets, ${from.first_name}?`)
+      );
     }
-})
+  }
+);
 
-mockBot.on('inline_query', e => {
-    const mockedText = e.query.length ? mockifyText(e.query) : mockifyText('Type something!')
-    const answer = [{
-        type: 'article',
-        id: String(Math.floor(Math.random() * 10000000000)),
-        title: mockifyText('KLIK HIER TRISTAN'),
-        input_message_content: {
-            message_text: mockedText,
-            parse_mode: 'Markdown'
-        },
-        description: mockedText
-    }]
-    mockBot.answerInlineQuery(e.id, answer)
-})
+mockBot.on("inline_query", e => {
+  const mockedText = e.query.length ? mock(e.query) : mock("Type something!");
+  const answer = [
+    {
+      type: "article",
+      id: String(Math.floor(Math.random() * 10000000000)),
+      title: mock("KLIK HIER TRISTAN"),
+      input_message_content: {
+        message_text: mockedText,
+        parse_mode: "Markdown"
+      },
+      description: mockedText
+    }
+  ];
+  mockBot.answerInlineQuery(e.id, answer);
+});
 
-mockBot.on('polling_error', console.error)
+mockBot.on("polling_error", console.error);
